821948f54c725e7246b022d5fe254f54d83794e2
itsgnegrao <itsg_negrao@hotmail.com>
2020-10-08 23:50:02 -0300

    true da true

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,9 @@
 import { postWithoutLogin } from "./request";
 
 export async function doLogin(email, senha) {
-  let link = process.env.REACT_APP_API_URL + "/api/login";
+  let link = process.env.REACT_APP_API_URL_APIV1 + "/login";
   let body = { username: email, password: senha };
 
   let resp = await postWithoutLogin(link, body);
-  return resp;
-}
-
-export async function doLogout() {
-  let link = "/api/logout";
-
-  let resp = await postWithoutLogin(link, null);
   return resp;
 }
